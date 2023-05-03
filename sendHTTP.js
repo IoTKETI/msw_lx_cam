7ca476140e51f7362eb9afa01b4aa7ebccb8c57c
dnjstjr93 <dnjstjr93@gmail.com>
2023-05-03 13:30:01 +0900

    - 사진 전송 에러 메시지 변경

--- sendHTTP.js
+++ sendHTTP.js
@@ -194,7 +194,7 @@ function send_image() {
                                     empty_count = 0;
                                     let msg = status + ' ' + count + ' ' + files[0];
                                     lib_mqtt_client.publish(my_status_topic, msg);
-                                    
+
                                     // 사진 이동
                                     // fs.rmSync('./' + geotagging_dir + '/' + files[0]);
                                     fs.renameSync('./' + geotagging_dir + '/' + files[0], './' + send_dir + '/' + files[0])
@@ -215,8 +215,11 @@ function send_image() {
                             })
                             .catch(function (error) {
                                 console.timeEnd('Send-' + files[0]);
-                                if (error.response)
-                                console.log('response: ', error.response);
+                                if (error.response) {
+                                    console.log('response: ', error.response);
+                                } else {
+                                    console.log('catch -', error)
+                                }
                                 // 전송 실패 시 현재 사진 계속 전송 시도
                                 setTimeout(send_image, 500);
                                 return
